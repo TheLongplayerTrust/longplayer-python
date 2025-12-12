eea7e68e0ab5c9dc00bb8bd7ed7c890475327c16
Daniel Jones <daniel@jones.org.uk>
2025-12-12 22:23:53 +0000

    Add --log flag

--- longplayer/__main__.py
+++ longplayer/__main__.py
@@ -1,13 +1,14 @@
 from longplayer import Longplayer, DEFAULT_AUDIO_GAIN, DEFAULT_BUFFER_SIZE
 from longplayer.utils import parse_icecast_config
 
-import configparser
 import sounddevice
 import argparse
 import logging
 import time
 import sys
 import os
+
+logger = logging.getLogger("longplayer")
 
 
 if __name__ == "__main__":
@@ -20,6 +21,7 @@ if __name__ == "__main__":
     parser.add_argument("--list-output-devices", action="store_true", help="List available audio output devices")
     parser.add_argument("--output-device", type=int, help="Selected audio output device (use system default if not specified)", default=None)
     parser.add_argument("--solo", type=int, help="Solo a specified layer, from 0 to 5", default=None)
+    parser.add_argument("--log", action="store_true", help="Enable logging to logs/longplayer.log")
     args = parser.parse_args()
 
     log_level = logging.INFO
@@ -29,13 +31,21 @@ if __name__ == "__main__":
         log_level = logging.WARNING
     logging.basicConfig(level=log_level, format="%(message)s")
 
+    if args.log:
+        os.makedirs("logs", exist_ok=True)
+        file_handler = logging.FileHandler("logs/longplayer.log")
+        file_handler.setLevel(logging.DEBUG)
+        
+        formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+        file_handler.setFormatter(formatter)
+        logging.getLogger().addHandler(file_handler)
+
     if args.list_output_devices:
         print(sounddevice.query_devices())
         sys.exit(1)
 
     # Parse Icecast configuration if available
     icecast_config = parse_icecast_config()
-
 
     try:
         longplayer = Longplayer(output_device=args.output_device,
@@ -48,5 +58,5 @@ if __name__ == "__main__":
         while True:
             time.sleep(0.1)
     except KeyboardInterrupt:
-        print("\nExiting Longplayer...")
+        logger.info("\nExiting Longplayer...")
         longplayer.stop()
